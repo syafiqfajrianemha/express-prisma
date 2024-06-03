4263d611a59490ce68f50e02c24282826fd2ca55
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2024-06-03 09:43:36 +0700

    `add` : auth jwt

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,55 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const PORT = 4000;
-
 const prisma = new PrismaClient();
 
 app.use(express.json());
+
+interface UserData {
+  id: string;
+  name: string;
+  email: string;
+  address: string;
+}
+
+interface ValidationRequest extends Request {
+  userData: UserData;
+}
+
+const accessValidation = (req: Request, res: Response, next: NextFunction) => {
+  const validationReq = req as ValidationRequest;
+  const { authorization } = validationReq.headers;
+
+  console.log("here: ", authorization);
+
+  if (!authorization) {
+    return res.status(401).json({
+      success: false,
+      message: "Token diperlukan",
+    });
+  }
+
+  const token = authorization.split(" ")[1];
+  const secret = process.env.JWT_SECRET!;
+
+  try {
+    const jwtDecode = jwt.verify(token, secret);
+
+    if (typeof jwtDecode !== "string") {
+      validationReq.userData = jwtDecode as UserData;
+    }
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
+  next();
+};
 
 app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
@@ -60,9 +102,22 @@ app.post("/login", async (req, res) => {
     });
   }
 
+  const payload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    address: user.address,
+  };
+
+  const secret = process.env.JWT_SECRET!;
+  const expiresIn = 60 * 60 * 1;
+
+  const token = jwt.sign(payload, secret, { expiresIn: expiresIn });
+
   return res.json({
     success: true,
     message: "Login successfully",
+    token: token,
   });
 });
 
@@ -83,7 +138,7 @@ app.post("/users", async (req, res) => {
   });
 });
 
-app.get("/users", async (req, res) => {
+app.get("/users", accessValidation, async (req, res) => {
   const users = await prisma.users.findMany({
     select: {
       id: true,
